a2e3fe18a22006da8e7e4334c2f7869451f6636a
wiz <aenor.realm@gmail.com>
2012-11-03 18:46:48 +0400

    show some logging

--- tests/JollyCloud.hs
+++ tests/JollyCloud.hs
@@ -1,8 +1,14 @@
 module Main where
 
 import qualified Control.Distributed.Backend.P2P as P2P
+import           Control.Distributed.Process as DP
+import           Control.Distributed.Process.Node as DPN
 
 import System.Environment (getArgs)
+import System.IO
+
+import Control.Monad
+import Control.Monad.Trans (liftIO)
 
 main :: IO ()
 main = do
@@ -10,4 +16,14 @@ main = do
 
   case args of
     ["-h"] -> putStrLn "Usage: jollycloud addr port [<seed>..]"
-    host:port:seeds -> P2P.bootstrap host port $ map P2P.makeNodeId seeds
+    host:port:seeds -> P2P.bootstrap host port (map P2P.makeNodeId seeds) mainProcess
+
+mainProcess :: LocalNode -> ProcessId -> Process ()
+mainProcess myNode peerContId = do
+    unregister "logger"
+    getSelfPid >>= register "logger"
+
+    forever $ do
+        (time, pid, msg) <- expect :: Process (String, ProcessId, String)
+        liftIO $ putStrLn $ time ++ " " ++ show pid ++ " " ++ msg
+        return ()
